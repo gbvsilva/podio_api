231894d8b56d8c3e60222032c34394bd9a32d7ba
gbvsilva <graco.babeuf18@gmail.com>
2022-08-11 08:55:30 -0300

    Treat db first connection

--- get_mydb.py
+++ get_mydb.py
@@ -2,12 +2,23 @@ from os import environ as env
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
+from get_time import getHour
+from telegram_tools import sendToBot
+
 def getDB():
-    mydb = psycopg2.connect(
-                host=env.get('POSTGRES_HOST'), 
-                user=env.get('POSTGRES_USERNAME'), 
-                password=env.get('POSTGRES_PASSWORD'), 
-                dbname=env.get('POSTGRES_DATABASE'), 
-                port=env.get('POSTGRES_PORT'))
-    mydb.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    return mydb
+    try:
+        mydb = psycopg2.connect(
+                    host=env.get('POSTGRES_HOST'), 
+                    user=env.get('POSTGRES_USERNAME'), 
+                    password=env.get('POSTGRES_PASSWORD'), 
+                    dbname=env.get('POSTGRES_DATABASE'), 
+                    port=env.get('POSTGRES_PORT'))
+        mydb.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    except psycopg2.Error as err:
+        # Inatividade do banco ou credenciais inválidas
+        message = f"{getHour()} -> Erro inesperado no acesso inicial ao BD. Terminando o programa. {err}"
+        print(message)
+        sendToBot(message)
+        exit(1)
+    else:
+        return mydb
