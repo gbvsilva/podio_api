ecda3e0dec3d2c93afa2d7f02c98a7009a2f4fab
gbvsilva <graco.babeuf18@gmail.com>
2022-11-17 15:34:02 -0300

    Better spacing

--- get_mydb.py
+++ get_mydb.py
@@ -3,9 +3,9 @@ import mysql.connector
 
 def getDB():
     mydb = mysql.connector.connect(
-                host=env.get('MYSQL_HOST'),
-                user=env.get('MYSQL_USER'),
-                password=env.get('MYSQL_PASSWORD'),
-                port=env.get('MYSQL_PORT')
-            )
+        host=env.get('MYSQL_HOST'),
+        user=env.get('MYSQL_USER'),
+        password=env.get('MYSQL_PASSWORD'),
+        port=env.get('MYSQL_PORT')
+    )
     return mydb
