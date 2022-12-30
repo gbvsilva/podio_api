59c163a77a59eb8b14f231d5920060b35d95177f
gbvsilva <graco.babeuf18@gmail.com>
2022-12-30 13:14:25 -0300

    Get rid of print

--- podio_tools.py
+++ podio_tools.py
@@ -1,39 +1,41 @@
 import json
 from get_time import getHour
 
+from logging_tools import logger
+
 def handlingPodioError(err):
     hour = getHour()
     message = ""
     if 'x-rate-limit-remaining' in err.status and err.status['x-rate-limit-remaining'] == '0':
-        message = f"{hour} -> Quantidade de requisições chegou ao limite por hora."
-        print(message)
+        message = f"Quantidade de requisições chegou ao limite por hora."
+        logger.info(message)
         return "rate_limit"
     if err.status['status'] == '401':
         # Token expirado. Re-autenticando
-        message = f"{hour} -> Token expirado. Renovando..."
-        print(message)
+        message = f"Token expirado. Renovando..."
+        logger.info(message)
         return "token_expired"
     if err.status['status'] == '400':
         if json.loads(err.content)['error_detail'] == 'oauth.client.invalid_secret':
-            message = f"{hour} -> Secret inválido."
+            message = f"Secret inválido."
         elif json.loads(err.content)['error_detail'] == 'user.invalid.username':
-            message = f"{hour} -> Usuário inválido."
+            message = f"Usuário inválido."
         elif json.loads(err.content)['error_detail'] == 'oauth.client.invalid_id':
-            message = f"{hour} -> ID do cliente inválido."
+            message = f"ID do cliente inválido."
         elif json.loads(err.content)['error_detail'] == 'user.invalid.password':
-            message = f"{hour} -> Senha do cliente inválido."
+            message = f"Senha do cliente inválido."
         else:
-            message = f"{hour} -> Parâmetro nulo na query. {err}"
-            print(message)
+            message = f"Parâmetro nulo na query. {err}"
+            logger.info(message)
             return "null_query"
         return "status_400"
     if err.status['status'] == '504':
-        message = f"{hour} -> Servidor demorou muito para responder. {err}"
-        print(message)
+        message = f"Servidor demorou muito para responder. {err}"
+        logger.info(message)
         return "status_504"
     else:
-        message = f"{hour} -> Erro inesperado no acesso a API. {err}"
-    print(message)
+        message = f"Erro inesperado no acesso a API. {err}"
+    logger.info(message)
     return "not_known_yet"
 
 
